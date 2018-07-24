346b8c6fa028f977cc79dd209a6839998b38e4c5
Shagul Beerkhan.S <shagulbeerkhan7@gmail.com>
2018-07-24 16:02:00 +0530

    Create sorted.py

---
+++ sorted.py
@@ -0,0 +1,8 @@
+n=int(input("enter the array size:"))
+list=[]
+for i in range(0,n):
+    newNo=int(input(''))
+    a=list.append(newNo)
+print("Actual list ",list)
+list.sort()
+print("Sorted list is",list)
